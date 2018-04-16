2006356db3a5b0056aa653287d804cb48c211dd5
Raymond <mr_me_9@hotmail.com>
2018-04-16 11:22:06 +0200

    Added entrypoints

---
+++ src/main/java/com/github/machosmurf/gswbankieren/bank/Bank.java
@@ -0,0 +1,11 @@
+package com.github.machosmurf.gswbankieren.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bank {
+    public static void main(String[] args) {
+        SpringApplication.run(Bank.class, args);
+    }
+}
---
+++ src/main/java/com/github/machosmurf/gswbankieren/central/CentralBank.java
@@ -0,0 +1,11 @@
+package com.github.machosmurf.gswbankieren.central;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralBank {
+    public static void main(String[] args) {
+        SpringApplication.run(CentralBank.class, args);
+    }
+}
