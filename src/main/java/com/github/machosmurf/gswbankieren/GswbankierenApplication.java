1582d82e1c42c4cbd0cf1e86e9263547f2b4b91b
Raymond <mr_me_9@hotmail.com>
2018-04-16 10:21:26 +0200

    Initial Commit

---
+++ src/main/java/com/github/machosmurf/gswbankieren/GswbankierenApplication.java
@@ -0,0 +1,12 @@
+package com.github.machosmurf.gswbankieren;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GswbankierenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GswbankierenApplication.class, args);
+	}
+}
