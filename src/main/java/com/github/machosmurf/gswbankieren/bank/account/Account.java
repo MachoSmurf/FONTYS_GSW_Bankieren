5a03394db6e5812d54c8f0f4e40f2ee28a75f4ae
Raymond <mr_me_9@hotmail.com>
2018-04-24 11:27:33 +0200

    added set of transactions to account

--- src/main/java/com/github/machosmurf/gswbankieren/bank/account/Account.java
+++ src/main/java/com/github/machosmurf/gswbankieren/bank/account/Account.java
@@ -1,9 +1,12 @@
 package com.github.machosmurf.gswbankieren.bank.account;
 
 import com.github.machosmurf.gswbankieren.bank.user.User;
+import com.github.machosmurf.gswbankieren.shared.Transaction;
 
 import javax.persistence.Entity;
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Set;
 
 @Entity
 public class Account {
@@ -11,14 +14,16 @@ public class Account {
     private String accountNumber;
     private User accountOwner;
     private double credit;
+    private Set<Transaction> transactions;
 
     public Account() {
     } //JPA Constructor
 
     public Account(String bankID, User accountOwner) {
-        credit = 500;
+        credit = 100;
         this.accountOwner = accountOwner;
         this.accountNumber = generateAccountNumber(bankID);
+        transactions = new HashSet<>();
     }
 
     private String generateAccountNumber(String bankID) {
